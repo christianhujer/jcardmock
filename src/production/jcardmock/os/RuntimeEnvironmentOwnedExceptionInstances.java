a2cf0dc690d8029d0a81772d148311c4a6348c76
Christian Hujer <cher@riedquat.de>
2015-04-09 02:49:37 +0530

    Prepare for CardException besides CardRuntimeException.

--- src/production/jcardmock/os/RuntimeEnvironmentOwnedExceptionInstances.java
+++ src/production/jcardmock/os/RuntimeEnvironmentOwnedExceptionInstances.java
@@ -1,24 +1,32 @@
 package jcardmock.os;
 
-import javacard.framework.CardRuntimeException;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RuntimeEnvironmentOwnedExceptionInstances {
-    private static Map<Class<? extends CardRuntimeException>, CardRuntimeException> runtimeEnvironmentOwnedExceptionInstances = new HashMap<>();
-    public static void throwIt(final Class<? extends CardRuntimeException> cardRuntimeExceptionClass, final short reason) {
-        final CardRuntimeException cardRuntimeException = runtimeEnvironmentOwnedExceptionInstances.computeIfAbsent(cardRuntimeExceptionClass, RuntimeEnvironmentOwnedExceptionInstances::throwNew);
-        cardRuntimeException.setReason(reason);
-        cardRuntimeException.fillInStackTrace();
-        throw cardRuntimeException;
+    private static Map<Class<? extends Exception>, Exception> runtimeEnvironmentOwnedExceptionInstances = new HashMap<>();
+    public static <E extends Exception> void throwIt(final Class<E> exceptionClass, final short reason) throws E {
+        final E exception = (E) runtimeEnvironmentOwnedExceptionInstances.computeIfAbsent(exceptionClass, RuntimeEnvironmentOwnedExceptionInstances::createNew);
+        setReason(exception, reason);
+        exception.fillInStackTrace();
+        throw exception;
     }
 
-    private static CardRuntimeException throwNew(final Class<? extends CardRuntimeException> aClass) {
+    private static void setReason(final Exception exception, final short reason) {
         try {
-            final Constructor<? extends CardRuntimeException> constructor = aClass.getConstructor(Short.TYPE);
+            final Method setReason = exception.getClass().getMethod("setReason", Short.TYPE);
+            setReason.invoke(exception, reason);
+        } catch (final InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    private static Exception createNew(final Class<? extends Exception> aClass) {
+        try {
+            final Constructor<? extends Exception> constructor = aClass.getConstructor(Short.TYPE);
             return constructor.newInstance((short) 0x0000);
         } catch (final NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
             throw new AssertionError(e);
